5bc9e321a7e97f9743ec5269b4d1fad8d6067b37
SleepyCatMeow <91329580+SleepyCatMeow@users.noreply.github.com>
2022-12-11 03:06:05 +0300

    1
    1

--- js/app.js
+++ js/app.js
@@ -4,5 +4,91 @@ const buttons = document.querySelectorAll("button");
 buttons.forEach(button => {
   button.addEventListener("click", () => {
     audio.play();
+    audio.playbackRate=1.5;
   });
 });
+
+//scroll
+$(document).ready(function (){
+    $("#click").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#walter").offset().top
+        }, 1000);
+    });
+});
+//
+$(document).ready(function (){
+  $("#sorry").click(function (){
+      $('html, body').animate({
+          scrollTop: $(".main").offset().top
+      }, 1000);
+  });
+});
+
+//popup
+$(function () {
+    "use strict";
+    
+    $(".popup img").click(function () {
+        var $src = $(this).attr("src");
+        $(".show").fadeIn();
+        $(".img-show img").attr("src", $src);
+    });
+    
+    $("span, .overlay").click(function () {
+        $(".show").fadeOut();
+    });
+    
+});
+
+
+
+//draw  https://youtu.be/amXSWXQssww
+
+var canvas, ctx, brush = {
+  x: 0, y: 0,
+  color: '#000000',
+  size: 10,
+  down: false,
+}, 
+strokes =  [],
+currentStroke = null;
+
+function redraw () {}
+
+function init () {
+  canvas = $('#draw');
+  canvas.attr({
+    // width: window.innerWidth,
+    // height: window.innerHeight
+  });
+  ctx = canvas.getContent('2d');
+
+  canvas.mousedown(function(e){
+    brush.down = true;
+    brush.x = e.pageX;
+    brush.y = e.pageY;
+
+    currentStroke = {
+      color: brush.color,
+      size: brush.size,
+      points: [],
+    };
+    currentStroke.points.push({
+      x: brush.x,
+      y: brush.y
+    });
+    redraw();
+  }).mouseup(function(e) {
+    brush.down = false;
+    brush.x = e.pageX;
+    brush.y = e.pageY;
+    currentStroke = {
+      color: brush.color,
+      size: brush.size,
+      points: [],
+    };
+  })
+}
+
+$(init);
